865cd2baa8f4dea3344b919dca934f46ed0c5de4
zengzhi2015 <zengzhi_2012@hotmail.com>
2017-10-12 17:06:20 +0800

    Add comments

--- Filter_test.m
+++ Filter_test.m
@@ -1,21 +1,20 @@
 close all
 clear
 clc
-%%
+%% read and convert image to double tpye
 img = imread('dayClip10--00000.png');
 img(img==0) = 1;
 img = double(img)/255;
 %% build filter
 red_filter = zeros(size(img));
 red_filter(:,:,2) = 1;
-%%
+%% calculate pixel direction
 pixel_direction = img;
 temp_norm = sqrt(pixel_direction(:,:,1).^2+pixel_direction(:,:,2).^2+pixel_direction(:,:,3).^2);
 for i=1:3
     pixel_direction(:,:,i) = pixel_direction(:,:,i)./temp_norm;
 end
-
-%%
+%% test combination
 
 %color_distance = sum(pixel_direction.*red_filter,3).*sum(img.*red_filter,3);
 %color_distance = sum(pixel_direction.*red_filter,3).*sum(img.*red_filter,3);
@@ -29,7 +28,7 @@ imshow(sum(pixel_direction.*red_filter,3).^2.*sum(img.*red_filter,3))
 %% 
 color_distance = sum(pixel_direction.*red_filter,3).^2.*sum(img.*red_filter,3);
 
-%%
+%% Create kernel
 kernal3 = [-1,-1,-1;
            -1, 8,-1;
            -1,-1,-1;]/8;
@@ -46,5 +45,4 @@ kernel_scaled(kernel_scaled<0) = -kernel_scaled(kernel_scaled<0)/sum(sum(kernel_
 
 disp(sum(kernel_scaled(kernel_scaled>=0)))
 disp(sum(kernel_scaled(kernel_scaled<0)))
-
 %% 
