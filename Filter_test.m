52cb824106fefb99718cbc4997c3c64c9899ea44
zengzhi2015 <zengzhi_2012@hotmail.com>
2017-10-12 20:26:24 +0800

    Git first push.

--- Filter_test.m
+++ Filter_test.m
@@ -2,12 +2,12 @@ close all
 clear
 clc
 %% read and convert image to double tpye
-img = imread('dayClip10--00028.png');
+img = imread('dayClip5--00015.png');
 img(img==0) = 1;
 img = double(img)/255;
 %% build filter
 red_filter = zeros(size(img));
-red_filter(:,:,3) = 1;
+red_filter(:,:,1) = 1;
 %% calculate pixel direction
 pixel_direction = img;
 temp_norm = sqrt(pixel_direction(:,:,1).^2+pixel_direction(:,:,2).^2+pixel_direction(:,:,3).^2);
@@ -32,7 +32,7 @@ figure(5)
 % color_distance = sum(pixel_direction.*red_filter,3).^2.*sum(img.*red_filter,3);
 color_distance = max(0,sum(pixel_direction.*red_filter,3)*2.5-1.5).*sum(img.*red_filter,3);%!!!!!!!!!!!!!!!
 color_distance_show = color_distance/max(max(color_distance));
-imshow(color_distance_show)
+imshow(color_distance)
 
 %% Create kernel
 kernal_hole = [ 0, 0,-1,-1,-1,-1, 0, 0;
@@ -43,7 +43,7 @@ kernal_hole = [ 0, 0,-1,-1,-1,-1, 0, 0;
            -1, 0, 0, 1, 1, 0, 0,-1;
             0,-1, 0, 0, 0, 0,-1, 0;
             0, 0,-1,-1,-1,-1, 0, 0;];
-ksize = [18,18];
+ksize = [30,30];
 kernel_scaled = imresize(kernal_hole,ksize,'nearest');
 kernel_scaled(kernel_scaled>=0) = kernel_scaled(kernel_scaled>=0)/sum(sum(kernel_scaled(kernel_scaled>=0)));
 kernel_scaled(kernel_scaled<0) = -1.5*kernel_scaled(kernel_scaled<0)/sum(sum(kernel_scaled(kernel_scaled<0))); %!!!!!!
@@ -59,4 +59,4 @@ result(result<0) = 0;
 disp(min(min(result)))
 disp(max(max(result)))
 result_show = result/max(max(result));
-imshow(result_show)
+imshow(result)
