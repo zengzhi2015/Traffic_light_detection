7e90e9f903d6e9f4a38b8ba072901030ac3711c6
zengzhi2015 <zengzhi_2012@hotmail.com>
2017-10-14 12:18:34 +0800

    Recalculated weighted channels

--- tensoeflow_version.py
+++ tensoeflow_version.py
@@ -41,51 +41,62 @@ def image_preprocessing(raw_image):
 def color_channel_extraction(image):
     v_channel = np.max(image,axis=2)
     s_channel = (v_channel-np.min(image,axis=2))/v_channel
-    r_channel = image[:,:,0];
-    g_channel = image[:,:,1];
-    y_channel = (image[:,:,0]+image[:,:,1])/2;
-    return v_channel, s_channel, r_channel, g_channel, y_channel
+    r_channel = image[:,:,0]
+    g_channel = image[:,:,1]
+    b_channel = image[:,:,2]
+    return v_channel, s_channel, r_channel, g_channel, b_channel
 
 #%% Apply color filter on the image
-def filtered_channels(image):
-    v_channel, s_channel, r_channel, g_channel, y_channel = color_channel_extraction(image)
+def weighted_channels(image):
+    v_channel, s_channel, r_channel, g_channel, b_channel = color_channel_extraction(image)
     weight_v = np.clip(v_channel*2-1,0,1)
     weight_s = np.clip(s_channel*2-1,0,1)
-    filtered_r = weight_v*weight_s*r_channel
-    filtered_g = weight_v*weight_s*g_channel
-    filtered_y = weight_v*weight_s*y_channel
-    return filtered_r,filtered_g,filtered_y
+    weighted_r = weight_v*weight_s*r_channel
+    weighted_g = weight_v*weight_s*g_channel
+    weighted_b = weight_v*weight_s*b_channel
+#    weighted_image = np.stack([weighted_r,weighted_g,weighted_b],axis=2)
+#    fig = plt.figure(figsize=(10,10))
+#    fig.add_subplot(2,2,1)
+#    plt.imshow(weighted_image, cmap='gray')
+#    fig.add_subplot(2,2,2)
+#    plt.imshow(weighted_r, cmap='gray')
+#    fig.add_subplot(2,2,3)
+#    plt.imshow(weighted_g, cmap='gray')
+#    fig.add_subplot(2,2,4)
+#    plt.imshow(weighted_b, cmap='gray')
+#    fig.tight_layout()
+    return weighted_r,weighted_g,weighted_b
+
+#%%
 #%% Simple classifier
-def simple_detector(image,show_figure=0):
-    filtered_r,filtered_g,filtered_y = filtered_channels(image)
-    if show_figure:
-        fig = plt.figure(figsize=(10,10))
-        fig.add_subplot(2,2,1)
-        plt.imshow(image)
-        fig.add_subplot(2,2,2)
-        plt.imshow(filtered_r)
-        fig.add_subplot(2,2,3)
-        plt.imshow(filtered_g)
-        fig.add_subplot(2,2,4)
-        plt.imshow(filtered_y)
-        fig.tight_layout()
+def simple_detector(image):
+    filtered_r,filtered_g,filtered_b = weighted_channels(image)
+#    fig = plt.figure(figsize=(10,10))
+#    fig.add_subplot(2,2,1)
+#    plt.imshow(image)
+#    fig.add_subplot(2,2,2)
+#    plt.imshow(filtered_r)
+#    fig.add_subplot(2,2,3)
+#    plt.imshow(filtered_g)
+#    fig.add_subplot(2,2,4)
+#    plt.imshow(filtered_b)
+#    fig.tight_layout()
     score_r = np.sum(filtered_r)
     score_g = np.sum(filtered_g)
-    score_y = np.sum(filtered_y)
-    print([score_r,score_g,score_y])
-    return np.argmax([score_r,score_g,score_y])
+    score_b = np.sum(filtered_b)
+    return np.argmax([score_r,score_g,score_b])
 #%% Define the computation graph
 
 
 #%% Overall test
 if __name__ == '__main__':
     # Image reading and preprocessing
-    # raw_image = mpimg.imread('dayClip10--00023.png')
+    raw_image = mpimg.imread('dayClip10--00023.png')
     # raw_image = mpimg.imread('dayClip5--00015.png')
     # raw_image = mpimg.imread('dayClip5--01177.png')
     # raw_image = mpimg.imread('img_10_575.png')
-    raw_image = mpimg.imread('img_12_388.png')
+    # raw_image = mpimg.imread('img_12_388.png')
     image = image_preprocessing(raw_image)
-    result = simple_detector(image,show_figure=1)
+    result = simple_detector(image)
     print('The detection result is {}.'.format(result))
     
